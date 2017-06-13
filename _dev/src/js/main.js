3b5b00c317d671d5e1162a41362c62f0ca9efa59
Qragon <qragon.odessa@gmail.com>
2017-06-13 17:09:33 +0300

    main page ready

--- _dev/src/js/main.js
+++ _dev/src/js/main.js
@@ -6,15 +6,15 @@ $(function () {
   Smooth_Scroll.init();
   
   // owl-carousel new
-  $(".owl-carousel[data-type='new']").owlCarousel({
+  $(".owl-carousel[data-type='category_new']").owlCarousel({
   	items: 1,
   	loop: true,
   	smartSpeed: 1000,
   	autoHeight: true,
   	nav: true,
   	navText: [
-  		"<span class='fa fa-chevron-left fa-2x owl-nav_arrow'></span>",
-  		"<span class='fa fa-chevron-right fa-2x owl-nav_arrow'></span>"
+  		"<span class='fa fa-chevron-left owl-nav_arrow'></span>",
+  		"<span class='fa fa-chevron-right owl-nav_arrow'></span>"
   	]
   });
 
@@ -26,8 +26,60 @@ $(function () {
   	autoHeight: true,
   	nav: true,
   	navText: [
-  		"<span class='fa fa-chevron-left fa-2x owl-nav_arrow'></span>",
-  		"<span class='fa fa-chevron-right fa-2x owl-nav_arrow'></span>"
+  		"<span class='fa fa-chevron-left owl-nav_arrow'></span>",
+  		"<span class='fa fa-chevron-right owl-nav_arrow'></span>"
+  	]
+  });
+
+  // owl-carousel videos
+  $(".owl-carousel[data-type='videos']").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	smartSpeed: 1000,
+  	autoHeight: true,
+  	nav: true,
+  	navText: [
+  		"<span class='fa fa-chevron-left owl-nav_arrow'></span>",
+  		"<span class='fa fa-chevron-right owl-nav_arrow'></span>"
+  	]
+  });
+
+  // owl-carousel products_new
+  $(".owl-carousel[data-type='products_new']").owlCarousel({
+  	items: 4,
+  	loop: true,
+  	smartSpeed: 1000,
+  	autoHeight: true,
+  	nav: true,
+  	navText: [
+  		"<span class='fa fa-chevron-left owl-nav_arrow'></span>",
+  		"<span class='fa fa-chevron-right owl-nav_arrow'></span>"
+  	]
+  });
+
+  // owl-carousel products_sale
+  $(".owl-carousel[data-type='products_sale']").owlCarousel({
+  	items: 4,
+  	loop: true,
+  	smartSpeed: 1000,
+  	autoHeight: true,
+  	nav: true,
+  	navText: [
+  		"<span class='fa fa-chevron-left owl-nav_arrow'></span>",
+  		"<span class='fa fa-chevron-right owl-nav_arrow'></span>"
+  	]
+  });
+
+  // owl-carousel products_offers
+  $(".owl-carousel[data-type='products_offers']").owlCarousel({
+  	items: 4,
+  	loop: true,
+  	smartSpeed: 1000,
+  	autoHeight: true,
+  	nav: true,
+  	navText: [
+  		"<span class='fa fa-chevron-left owl-nav_arrow'></span>",
+  		"<span class='fa fa-chevron-right owl-nav_arrow'></span>"
   	]
   });
 });
