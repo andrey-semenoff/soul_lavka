75e625a0065cca630a947bd3639e9d257e4ce20c
Qragon <qragon.odessa@gmail.com>
2017-06-14 18:13:20 +0300

    catalog, gallery

--- _dev/src/js/main.js
+++ _dev/src/js/main.js
@@ -19,7 +19,7 @@ $(function () {
   });
 
   // owl-carousel category
-  $(".owl-carousel[data-type='category']").owlCarousel({
+  $(".owl-carousel[data-type='catalog']").owlCarousel({
   	items: 3,
   	loop: true,
   	smartSpeed: 1000,
@@ -82,6 +82,22 @@ $(function () {
   		"<span class='fa fa-chevron-right owl-nav_arrow'></span>"
   	]
   });
+	
+	//
+	$(".filters__input").on("change", function (e) {
+		var $this = $(this),
+			checked = $this.is(':checked'),
+			$item = $this.parents(".filters__item"),
+			$icon = $item.find(".icon");
+
+		if( checked ) {
+			$item.addClass("filters__item_active");
+			$icon.removeClass("icon-checkbox_false").addClass("icon-checkbox_true");
+		} else {
+			$item.removeClass("filters__item_active");
+			$icon.removeClass("icon-checkbox_true").addClass("icon-checkbox_false");
+		}
+	});
 });
 
 
